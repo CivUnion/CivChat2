939882a1440d9fb1180f450154aebcddb1f25f37
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-09-14 17:46:20 -0600

    Remove CivGradle

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,18 +1,10 @@
 plugins {
-	`java-library`
-	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
-}
-
-civGradle {
-	paper {
-		pluginName = "CivChat2"
-	}
+	id("io.papermc.paperweight.userdev") version "1.3.8"
 }
 
 dependencies {
 	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-	compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
-	compileOnly("net.civmc.namelayer:paper:3.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc.civmodcore:CivModCore:2.4.0:dev-all")
+	compileOnly("net.civmc.namelayer:NameLayer:3.1.0:dev")
 }
